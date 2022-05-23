2f4eb30dd2c64055f3421b77ac46db1109c1f10e
Tiago138 <tgnvsctn@gmail.com>
2022-05-23 14:37:06 -0300

    feat: created textcomponent

---
+++ src/components/TextComponent/stories.jsx
@@ -0,0 +1,23 @@
+import { TextComponent } from '.';
+
+export default {
+  title: 'TextComponet',
+  component: TextComponent,
+  args: {
+    children: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+     Accusantium tenetur iusto sunt fuga aspernatur nisi praesentium incidunt
+     inventore unde reiciendis doloribus, optio earum id ut totam, alias adipisci
+     eveniet ipsum.`,
+  },
+  argTypes: {
+    children: { type: 'string' },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <TextComponent {...args} />
+    </div>
+  );
+};
