28d1472b340fc1d5f487f2170fd7b56cbc0842cf
Tiago138 <tgnvsctn@gmail.com>
2022-05-23 13:03:51 -0300

    feat: created heading component

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 
 import Home from './templates/Home';
+
+import { GlobalStyles } from './styles/global-styles';
+import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>,
 );
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -2,6 +2,7 @@ function Home() {
   return (
     <div className="Home">
       <h1>hello</h1>
+      <span>teste</span>
     </div>
   );
 }
