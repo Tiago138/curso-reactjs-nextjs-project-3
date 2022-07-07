014f42be2da7686482c43c77d015c90da7756306
Tiago138 <tgnvsctn@gmail.com>
2022-07-07 14:44:14 -0300

    feat: created grid two columns component

---
+++ src/components/GridTwoColum/mock.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Grid Two Columns',
+  text: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis nobis id possimus similique iste cumque dicta voluptatem consequatur obcaecati consectetur praesentium rem officiis perferendis vitae provident, cupiditate nesciunt, incidunt laudantium?`,
+  srcImg: 'assets/images/javascript.svg',
+};
---
+++ src/components/GridTwoColum/styles.js
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+import { Title } from '../Heading/styles';
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    gap: ${theme.spacings.large};
+
+    @media ${theme.media.lteMedium} {
+      grid-template-columns: 1fr;
+      gap: ${theme.spacings.xhuge};
+
+      text-align: center;
+    }
+
+    ${Title} {
+      margin-bottom: ${theme.spacings.xlarge};
+    }
+  `}
+`;
+
+export const TextContainer = styled.div`
+  ${({ theme }) => css``}
+`;
+
+export const ImageContainer = styled.div`
+  ${({ theme }) => css``}
+`;
+
+export const Image = styled.img`
+  ${({ theme }) => css``}
+`;
